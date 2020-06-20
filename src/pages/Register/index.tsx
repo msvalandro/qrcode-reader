8182624f19757059414d0c99af3e74f849cafe64
Matheus Valandro <msvalandro@gmail.com>
2020-06-20 18:33:46 -0300

    fix: change facingMode for camera

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -142,6 +142,7 @@ const Register: React.FC = () => {
           onError={handleError}
           onScan={handleScan}
           style={{ width: '100%' }}
+          facingMode="user"
         />
 
         <span />
